6d75712345d5dd4e411f95294a3a66a8481a3f91
Kensuke Nagae <kyanny@gmail.com>
2014-08-23 02:14:15 +0900

    v2
    * thor + octokit
    * --all option (auto pagination)
    * new `ghn open` command (--open option is osbolete)

---
+++ lib/ghn/collector.rb
@@ -0,0 +1,24 @@
+class Ghn
+  class Collector
+    def initialize(client, repo_full_name = nil)
+      @client = client
+      @repo_full_name = repo_full_name
+    end
+
+    def collect(page = 1)
+      notifications = if @repo_full_name
+                        @client.repo_notifications(@repo_full_name, page: page)
+                      else
+                        @client.notifications(page: page)
+                      end
+      @count = notifications.count
+      notifications.map { |notification|
+        Notification.new(notification).to_url
+      }
+    end
+
+    def has_next?
+      @count == 50
+    end
+  end
+end
