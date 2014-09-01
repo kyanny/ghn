b1f2184d60937d8555e0521a2d2cbc940546a006
Kensuke Nagae <kyanny@gmail.com>
2014-09-01 16:52:54 +0900

    Make "follow issuecomment" feature optional

--- lib/ghn/collector.rb
+++ lib/ghn/collector.rb
@@ -1,19 +1,22 @@
 class Ghn
   class Collector
-    def initialize(client, repo_full_name = nil)
+    attr_reader :client, :repo_full_name, :follow_issuecomment
+
+    def initialize(client, repo_full_name = nil, follow_issuecomment = false)
       @client = client
       @repo_full_name = repo_full_name
+      @follow_issuecomment = follow_issuecomment
     end
 
     def collect(page = 1)
-      notifications = if @repo_full_name
-                        @client.repo_notifications(@repo_full_name, page: page)
+      notifications = if repo_full_name
+                        client.repo_notifications(repo_full_name, page: page)
                       else
-                        @client.notifications(page: page)
+                        client.notifications(page: page)
                       end
       @count = notifications.count
       notifications.map { |notification|
-        Notification.new(notification).type_class.new(notification).url
+        Notification.new(notification, follow_issuecomment).type_class.new(notification, follow_issuecomment).url
       }.compact
     end
 
